443950bd910a73e72fccad9376269c383927bb6f
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-10-30 17:01:49 +0200

    Check if two Strings are anagrams of each other

--- src/String/Anagram.java
+++ src/String/Anagram.java
@@ -1,6 +1,8 @@
 package String;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
 
 //Check if two Strings are anagrams of each other
 //to solve it  just yot need to check if equal contains of two strings
@@ -11,10 +13,68 @@ public class Anagram {
         }
         char[] aChars = a.toCharArray();
         char[] bChars = b.toCharArray();
-        Arrays.sort(aChars);
-        Arrays.sort(bChars);
-        return Arrays.equals(aChars,bChars);
+        return Arrays.equals(aChars,bChars);//check equal contains
     }
+    public static boolean isAnagram1(String a, String b)//best way
+    {
+        // Check if length of both strings is same or not
+        if (a.length() != b.length()) {
+            return false;
+        }
+        // Create a HashMap containing Character as Key and
+        // Integer as Value. We will be storing character as
+        // Key and count of character as Value.
+        HashMap<Character, Integer> map = new HashMap<>();
+        // Loop over all character of String a and put in
+        // HashMap.
+        for (int i = 0; i < a.length(); i++) {
+            // Check if HashMap already contain current
+            // character or not
+            if (map.containsKey(a.charAt(i))) {
+                // If contains increase count by 1 for that
+                // character
+                map.put(a.charAt(i),
+                        map.get(a.charAt(i)) + 1);
+            }
+            else {
+                // else put that character in map and set
+                // count to 1 as character is encountered
+                // first time
+                map.put(a.charAt(i), 1);
+            }
+        }
+        // Now loop over String b
+        for (int i = 0; i < b.length(); i++) {
+            // Check if current character already exists in
+            // HashMap/map
+            if (map.containsKey(b.charAt(i))) {
+                // If contains reduce count of that
+                // character by 1 to indicate that current
+                // character has been already counted as
+                // idea here is to check if in last count of
+                // all characters in last is zero which
+                // means all characters in String a are
+                // present in String b.
+                map.put(b.charAt(i),
+                        map.get(b.charAt(i)) - 1);
+            }
+            else {
+                return false;
+            }
+        }
+        // Extract all keys of HashMap/map
+        Set<Character> keys = map.keySet();
+        // Loop over all keys and check if all keys are 0.
+        // If so it means it is anagram.
+        for (Character key : keys) {
+            if (map.get(key) != 0) {
+                return false;
+            }
+        }
+        // Returning True as all keys are zero
+        return true;
+    }
+
     public static void main(String[] args) {
         String a = "listen";
         String b = "silent";
